28e52193b94893968c8668c89ed0c57f061009a3
Matteo Bogo <matteo.bogo@gmail.com>
2019-06-09 16:23:46 +0200

    Added remote docker debugging environment - added a dockerfile dev for building a toskose-manager for dev purposes only - added a docker-compose-dev for deploying a multi-component app for testing the toskose-manager - added a script (will be deprecated) for launching a dev toskose manager  locally - updated the dockerignore

--- boot-dev.sh
+++ boot-dev.sh
@@ -1,18 +1,67 @@
 #!/bin/bash
+set -e
 
-#echo "Configure virtualenv and flask environment.."
-#source venv/bin/activate
-#export FLASK_APP=app/run.py
-#export FLASK_ENV=development
-#echo "List of available endpoints.."
-#flask routes
-#echo "Starting the development server.."
-#flask run
+# absolute path of current script
+ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source venv/bin/activate
-export FLASK_ENV=development
+DEFAULT_DOCKER_COMPOSE=tests/data/thinking/docker-compose-without-manager.yml
+DEFAULT_MANIFEST=tests/data/thinking/config/
+DEFAULT_TOSKOSE_CONFIG=tests/data/thinking/manifest/
+
+DEFAULT_LOGS_PATH=$ROOT_DIR/logs/
+
+shutdown () {
+    echo "Shutdown."
+    docker-compose -f $ROOT_DIR/$DOCKER_COMPOSE down
+}
+
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "Detected CTRL-C."
+    shutdown
+}
+
+if [ ! -f "$DOCKER_COMPOSE" ] || [ ! -d "$MANIFEST" ] || [ ! -d "$TOSKOSE_CONFIG" ]; then
+    echo "missing docker-compose file, manifest dir or toskose config dir."
+    DOCKER_COMPOSE=$DEFAULT_DOCKER_COMPOSE
+    MANIFEST=$DEFAULT_MANIFEST
+    TOSKOSE_CONFIG=$DEFAULT_TOSKOSE_CONFIG
+    printf "Default application Thinking loaded.\n${DOCKER_COMPOSE}\n${MANIFEST}\n${TOSKOSE_CONFIG}\n"
+fi
+
+if [ ! -d "$LOGS_PATH" ]; then
+    echo "missing log path, creating one.."
+    LOGS_PATH=$DEFAULT_LOGS_PATH
+    mkdir -p $LOGS_PATH
+fi
+
+if [ ! -d ".venv" ]; then
+    echo 'missing virtualenv, creating one..'
+    python3 -m venv .venv
+fi
+
+# TODO check docker/docker-compose installed?
+
+source .venv/bin/activate
+pip install -r requirements.txt
+
+echo "Starting the docker-compose file.."
+docker-compose -f $DOCKER_COMPOSE up -d
+
+export TOSKOSE_LOGS_PATH=$LOGS_PATH
 export TOSKOSE_APP_MODE=development
+export FLASK_ENV=development
 export FLASK_APP=run.py
+
+export TOSKOSE_CONFIG_PATH=$TOSKOSE_CONFIG_PATH
+export TOSKOSE_MANIFEST_PATH=$MANIFEST
+
 cd app
-python -m flask routes
-python -m flask run
+if ! python -m flask routes; then
+    shutdown
+fi
+
+if ! python -m flask run; then
+    shutdown
+fi
