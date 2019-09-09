20e4de23e63a83bb4b3aee6eca22cb09a41398c7
Matteo Bogo <matteo.bogo@gmail.com>
2019-09-09 02:13:07 +0200

    removed docker client impl. and client config

--- app/client/client.py
+++ app/client/client.py
@@ -1,11 +1,9 @@
 from enum import Enum, auto
 from app.client.impl.xmlrpc_client import ToskoseXMLRPCclient
-from app.client.impl.docker_client import DockerClient
 
 
 class ProtocolType(Enum):
     XMLRPC = auto()
-    DOCKER = auto()
 
 class ToskoseClientFactory:
 
@@ -14,7 +12,5 @@ class ToskoseClientFactory:
         protocol_type = protocol_type.upper()
         if protocol_type == ProtocolType.XMLRPC.name:
             return ToskoseXMLRPCclient(**kwargs)
-        elif protocol_type == ProtocolType.DOCKER.name:
-            return DockerClient(**kwargs, standalone=True)
         else:
             raise ValueError("Invalid client protocol: {}".format(protocol_type))
